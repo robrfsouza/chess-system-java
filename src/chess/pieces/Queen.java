fe1cce7a9847bdcf59609e52d6371bbfb4fd830d
Robson França <robrfs@gmail.com>
2022-01-02 23:38:32 -0300

    ChessException and ChessPosition

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,19 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+		// TODO Stub de construtor gerado automaticamente
+	}
+	
+	public String toString() {
+		return "Q";
+	}
+
+	
+}
